720174d1e38ee77ea4095cec67d57b1055057374
Frozi <Frozik6k@yandex.ru>
2025-09-19 22:56:32 +0500

    убрал лишние импорты

--- src/main/java/ru/Frozik6k/service/UserService.java
+++ src/main/java/ru/Frozik6k/service/UserService.java
@@ -1,15 +1,9 @@
 package ru.Frozik6k.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import ru.Frozik6k.repository.UserRepository;
 import ru.Frozik6k.dto.UserDto;
-import ru.Frozik6k.mapper.UserMapper;
-import ru.Frozik6k.model.User;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public interface UserService {
