17c5f400b76f4ddd0dd5f7d5dd5885bdf9fa02a5
Frozi <Frozik6k@yandex.ru>
2025-09-20 23:59:24 +0500

    Убрал из тестирование БД, чтобы тестирование контекста проходило

--- user-service/src/test/java/ru/Frozik6k/AstonSpecialApplicationTests.java
+++ user-service/src/test/java/ru/Frozik6k/AstonSpecialApplicationTests.java
@@ -3,9 +3,20 @@ package ru.Frozik6k;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import ru.Frozik6k.repository.UserRepository;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude=" +
+                "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+                "org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration," +
+                "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
+                "org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration"
+})
 public class AstonSpecialApplicationTests {
+
+    @MockitoBean
+    private UserRepository userRepository;
 
     @Test
     void contextLoads() {
